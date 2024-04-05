80386536041b6d0c724eb3190ac6e9d73462344a
Duro <duro.dp02@gmail.com>
2024-04-05 03:10:46 -0600

    *cors

--- relay-cms/config/middlewares.js
+++ relay-cms/config/middlewares.js
@@ -1,19 +1,18 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
   {
-    name: 'strapi::cors',
+    name: "strapi::cors",
     config: {
       enabled: true,
-      origin: process.env.CORS_ORIGINS ? process.env.CORS_ORIGINS.split(',') : ['http://localhost:3000', 'https://relay-nine.vercel.app', '*'], // Add your allowed origins here
-      // You can customize other CORS settings here
+      origin: "*", // Allow all origins
     },
   },
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
