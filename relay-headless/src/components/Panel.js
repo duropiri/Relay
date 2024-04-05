944694effe045e4b5812eb9df7b986748d421eb9
Duro <duro.dp02@gmail.com>
2024-04-05 04:17:21 -0600

    mobile++

--- relay-headless/src/components/Panel.js
+++ relay-headless/src/components/Panel.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/all";
 import { useGlobalState } from "./GlobalStateContext";
