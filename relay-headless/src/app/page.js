7bf69889f70e4ffe8f22464636571afdd5333bec
Durotimi Duro-Pirisola <duro@Durotimis-MacBook-Pro.local>
2024-05-05 18:41:18 -0600

    +

--- relay-headless/src/app/page.js
+++ relay-headless/src/app/page.js
@@ -7,10 +7,25 @@ import Why from "../components/Why";
 import React from "react";
 import Head from "next/head";
 
-
 const Home = () => {
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" href="/img/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/img/favicon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/img/favicon.png"
+        />
+      </Head>
       <div className="flex flex-col gap-10">
         <Hero />
         <Why />
