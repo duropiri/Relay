609a46f7951476847283f1715ff7501b5c27f9af
Durotimi Duro-Pirisola <duro@Durotimis-MacBook-Pro.local>
2024-05-05 21:58:50 -0600

    ...

--- relay-headless/src/components/Hero.js
+++ relay-headless/src/components/Hero.js
@@ -172,7 +172,7 @@ const Hero = () => {
               decoding="async"
             />
           </div> */}
-          <div className="absolute w-[100vw] h-[100vh] brightness-[0.3] -top-20 sm:-top-10 left-0 pointer-events-none sm:pointer-events-auto" data-speed="0.6">
+          <div className="absolute w-[100vw] h-[100vh] brightness-[0.3] -top-20 sm:-top-32 left-0 pointer-events-none sm:pointer-events-auto" data-speed="0.6">
             <Spline
               scene="https://prod.spline.design/jihRugpNkwT9B6KT/scene.splinecode"
               className=""
--- relay-headless/src/components/SmoothScrolling.js
+++ relay-headless/src/components/SmoothScrolling.js
@@ -3,7 +3,7 @@ import { ReactLenis } from "@studio-freight/react-lenis";
 
 function SmoothScrolling({ children }) {
   return (
-    <ReactLenis root options={{ lerp: 0.075, duration: 1, smoothTouch: true }}>
+    <ReactLenis root options={{ lerp: 0.2, duration: 1, smoothTouch: true }}>
       {children}
     </ReactLenis>
   );
