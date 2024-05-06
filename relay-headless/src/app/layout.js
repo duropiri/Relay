c61c18526593d3f2cdaa1d1b7445dc8b1764d060
Durotimi Duro-Pirisola <duro@Durotimis-MacBook-Pro.local>
2024-05-05 18:50:47 -0600

    +share images

--- relay-headless/src/app/layout.js
+++ relay-headless/src/app/layout.js
@@ -20,11 +20,45 @@ export const metadata = {
   viewport: "width=device-width, initial-scale=1.0",
   robots: "index, follow",
   image: "/img/social-media-share-preview.png",
+  url: "http://relaymedia.agency",
 };
 
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords} />
+        <meta charSet={metadata.charset} />
+        <meta name="viewport" content={metadata.viewport} />
+        <meta name="robots" content={metadata.robots} />
+
+        <link rel="shortcut icon" href="/img/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/img/favicon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/img/favicon.png"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={metadata.url} />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content={metadata.image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:url" content={metadata.url} />
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+        <meta name="twitter:image" content={metadata.image} />
+      </head>
       <body className={inter.className}>
         <Analytics />
         <GlobalStateProvider>
--- relay-headless/src/app/page.js
+++ relay-headless/src/app/page.js
@@ -6,11 +6,19 @@ import Services from "../components/Services";
 import Why from "../components/Why";
 import React from "react";
 import Head from "next/head";
+import { metadata } from "./layout";
 
 const Home = () => {
   return (
     <>
-      <Head>
+      {/* <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords} />
+        <meta charSet={metadata.charset} />
+        <meta name="viewport" content={metadata.viewport} />
+        <meta name="robots" content={metadata.robots} />
+
         <link rel="shortcut icon" href="/img/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon.png" />
         <link
@@ -25,7 +33,17 @@ const Home = () => {
           sizes="16x16"
           href="/img/favicon.png"
         />
-      </Head>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={metadata.url} />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content={metadata.image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:url" content={metadata.url} />
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+        <meta name="twitter:image" content={metadata.image} />
+      </Head> */}
       <div className="flex flex-col gap-10">
         <Hero />
         <Why />
