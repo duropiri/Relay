eccd0e7421c9eeafcdb0f2b2c022db75ee032e97
Duro <duro.dp02@gmail.com>
2024-06-11 20:28:05 -0600

    hotjar

--- relay-headless/src/app/layout.js
+++ relay-headless/src/app/layout.js
@@ -114,6 +114,20 @@ const RootLayout = ({ children }) => {
             })(window,document,'script','dataLayer','GTM-PXXTT7FJ');`,
           }}
         ></Script>
+
+        <Script
+          id="hotjar"
+          dangerouslySetInnerHTML={{
+            __html: `(function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:5021016,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+          }}
+        ></Script>
       </head>
       <body
       // className={inter.className}
