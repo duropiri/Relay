e2bc9abc0b821dbd55339c2ba8dd28264fb880e4
Duro <duro.dp02@gmail.com>
2024-06-17 22:17:47 -0600

    remove calendly

--- relay-headless/src/components/layouts/home/sections/CalendlyEmbed.js
+++ relay-headless/src/components/layouts/home/sections/CalendlyEmbed.js
@@ -25,12 +25,12 @@ const CalendlyEmbed = () => {
                 data-url="https://calendly.com/relaydigitalyyc/discovery"
               ></div>
             </div>
-            <Script
+            {/* <Script
               type="text/javascript"
               src="https://assets.calendly.com/assets/external/widget.js"
               async
               strategy="afterInteractive"
-            ></Script>
+            ></Script> */}
           </div>
           <div className="relative flex items-center justify-center w-full md:w-1/2 h-full md:p-6 overflow-hidden">
             <div className="ring-1 ring-white/10 bg-neutral-950 rounded-3xl p-8 xl:p-10">
